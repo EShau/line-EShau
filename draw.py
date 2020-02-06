0645a1376f2498198e59b22ae9ca51caf8646641
EShau <EShau00@stuy.edu>
2020-02-06 11:51:48 -0500

    yay

--- draw.py
+++ draw.py
@@ -4,11 +4,24 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if x0 != min(x0, x1):
         x0, x1 = x1, x0
         y0, y1 = y1, y0
+    print(x0, y0)
+    print(x1, y1)
     x, y = int(x0), int(y0)
-    m = (y1 - y0) / (x1 - x0)
-    if m > 0:
+    m = (y1 - y0) * 1.0 / (x1 - x0)
+    print(m)
+    if m >= 0:
         if m > 1:
-            return None
+            A = y1 - y0
+            B = -(x1 - x0)
+            d = 2 * A + B
+            while x <= x1:
+                # print("(%d, %d)", x, y)
+                plot(screen, color, x, y)
+                if d > 0:
+                    y += 1
+                    d += (2 * B)
+                x += 1
+                d += (2 * A)
         else:
             A = y1 - y0
             B = -(x1 - x0)
